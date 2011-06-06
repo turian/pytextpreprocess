42dbbfb896ac4484f69bdde1ac5326cebbf1fefe
Joseph Turian <turian@gmail.com>
2011-06-06 10:24:58 +0000

    Broke text preprocessing into helper functions

--- textpreprocess.py
+++ textpreprocess.py
@@ -26,8 +26,6 @@ def textpreprocess(txt, converthtml=True, sentencetokenize=True, removeblankline
     Note: We use the Porter stemmer. (Optimization: Shouldn't rebuild
     the PorterStemmer object each time this function is called.)
     """
-    global stoplist
-
     if converthtml:
         txt = common.html2text.html2text(txt)
 
@@ -57,33 +55,49 @@ def textpreprocess(txt, converthtml=True, sentencetokenize=True, removeblankline
         txtwords = [[string.lower(w) for w in t] for t in txtwords]
 
     if removestopwords:
-#        stoplist = stopwords.words("english")
-        if stoplist is None:
-            stoplist = [string.strip(l) for l in open(STOPFILE).readlines()]
-        txtwords = [[w for w in t if w not in stoplist] for t in txtwords]
+        txtwords = _removestopwords(txtwords)
 
     if stem:
-        stemmer = PorterStemmer()
-        txtwords = [[stemmer.stem(w) for w in t] for t in txtwords]
+        txtwords = _stem(txtwords)
 
     # TODO: Maybe remove Unicode accents? http://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
 
     if removenonalphanumericchars:
-        alphanumre = re.compile("[\w\-\' ]", re.UNICODE)
-        txtwords = [[string.join([c for c in w if alphanumre.search(c) is not None], "") for w in t] for t in txtwords]
+        txtwords = _removenonalphanumericchars(txtwords)
 
     txtwords = [[w for w in t if w != ""] for t in txtwords]
 
     if stemlastword:
-        stemmer = PorterStemmer()
-        txtwords = [t[:-1] + [stemmer.stem(t[-1])] for t in txtwords if len(t) > 0]
+        txtwords = _stemlastword(txtwords)
 
     txts = [string.join(words) for words in txtwords]
 
     if stripallwhitespace:
-        txts = [_wsre.sub("", txt) for txt in txts]
+        txts = _stripallwhitespace(txts)
 
     return string.join(txts, sep="\n")
+
+def _removestopwords(txtwords):
+    global stoplist
+#    stoplist = stopwords.words("english")
+    if stoplist is None:
+        stoplist = [string.strip(l) for l in open(STOPFILE).readlines()]
+    return [[w for w in t if w not in stoplist] for t in txtwords]
+
+def _stem(txtwords):
+    stemmer = PorterStemmer()
+    return [[stemmer.stem(w) for w in t] for t in txtwords]
+
+def _removenonalphanumericchars(txtwords):
+    alphanumre = re.compile("[\w\-\' ]", re.UNICODE)
+    return [[string.join([c for c in w if alphanumre.search(c) is not None], "") for w in t] for t in txtwords]
+
+def _stemlastword(txtwords):
+    stemmer = PorterStemmer()
+    return [t[:-1] + [stemmer.stem(t[-1])] for t in txtwords if len(t) > 0]
+
+def _stripallwhitespace(txts):
+    return [_wsre.sub("", txt) for txt in txts]
 
 if __name__ == "__main__":
     import sys
